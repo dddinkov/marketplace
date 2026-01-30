cedc0883e0ffeb51412e9124b32eb83b90b87c30
diqn <didkot0334@gmail.com>
2026-01-30 21:32:57 +0200

    Modified the way .env files work in Spring... No it's .env.properties

--- src/main/java/com/market/marketplace/MarketplaceApplication.java
+++ src/main/java/com/market/marketplace/MarketplaceApplication.java
@@ -2,13 +2,11 @@ package com.market.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MarketplaceApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load(); // Loads variables from .env file
 		SpringApplication.run(MarketplaceApplication.class, args);
 	}
 

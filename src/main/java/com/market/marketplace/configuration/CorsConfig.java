1a45642194773d81804ebef01d4bef4513d70f04
diqn <didkot0334@gmail.com>
2026-02-18 01:50:59 +0200

    Removed unnecessary code.

--- src/main/java/com/market/marketplace/configuration/CorsConfig.java
+++ src/main/java/com/market/marketplace/configuration/CorsConfig.java
@@ -3,8 +3,6 @@ package com.market.marketplace.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,6 +21,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(frontendOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };

28fcab6d5f2f5cd2619eaee3abd3e35dde3d73ad
diqn <didkot0334@gmail.com>
2026-02-18 01:47:34 +0200

    Added a simple header.

---
+++ frontend/src/pages/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import MiniCart from "./MiniCart";
+import "../styles/Header.css";
+
+export default function Header({ cartItemCount = 0 }) {
+  return (
+    <header>
+      <div>
+        Marketplace
+      </div>
+      <nav>
+        <Link className="link" to="/">Home</Link>
+        
+        <Link className="link" to="/me">My Profile</Link>
+      </nav>
+      <div className="mini-cart-container">
+        <MiniCart />
+      </div>
+    </header>
+  );
+}   

ad31f121c41dddc13702ff320c39a2d79861fa4b
diqn <didkot0334@gmail.com>
2026-02-23 19:08:46 +0200

    Added GET all categories

--- frontend/src/api/category.js
+++ frontend/src/api/category.js
@@ -1,17 +1,14 @@
-import { API_URL } from "./api.js";
+import {API_URL, authHeaders, headers} from "./api.js";
 
 export async function addCategory(name) {
     const response = await fetch(`${API_URL}/category/add`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("token")}`
-        },
+        headers: authHeaders(),
         body: JSON.stringify({ name })
     });
 
     if (!response.ok) {
-        const error = await res.json();
+        const error = await response.json();
         throw new Error(error.message || "Failed to add category");
     }
 
@@ -19,17 +16,28 @@ export async function addCategory(name) {
 }
 
 export async function getCategory(categoryId) {
-    const response = await fetch(`${API_URL}/products/${categoryId}`, {
+    const response = await fetch(`${API_URL}/category/${categoryId}`, {
         method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem("token")}`
-        }
+        headers: authHeaders()
     });
 
     if (!response.ok) {
-        const error = await res.json();
+        const error = await response.json();
         throw new Error(error.message || "Failed to fetch category");
+    }
+
+    return response.json();
+}
+
+export async function getCategories() {
+    const response = await fetch(`${API_URL}/category/all`, {
+        method: "GET",
+        headers: authHeaders()
+    }
+)
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message || "Failed to fetch categories");
     }
 
     return response.json();

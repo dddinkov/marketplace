dcc65d93a2429649228755bd1b79f1cf0010866b
diqn <didkot0334@gmail.com>
2026-02-26 09:21:16 +0200

    Allowing unauthorized requests to filter items by categories

--- src/main/java/com/market/marketplace/configuration/SecurityConfig.java
+++ src/main/java/com/market/marketplace/configuration/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/category/*/products", "/category/all").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);

8f7839ab489d50f2cafed59ba70807a25ecab322
diqn <didkot0334@gmail.com>
2026-03-01 13:02:32 +0200

    Integrated pages to avoid fetching all products at once

--- src/main/java/com/market/marketplace/repository/ProductRepository.java
+++ src/main/java/com/market/marketplace/repository/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.market.marketplace.repository;
 
 import com.market.marketplace.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByCategoryId(Long categoryId);
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 }

e372d1cb4d82b3b3605ad7bf2332c80123adc2eb
diqn <didkot0334@gmail.com>
2026-05-21 10:56:28 +0300

    Adding MessageList component to show the messages.

---
+++ frontend/src/pages/MessageList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/MessageList.css";
+
+const MessageList = ({ messages }) => {
+    return (
+        <div className="message-list">
+            {messages.map((msg, index) => (
+                <div key={index} className="message">
+                    <strong>{msg.senderEmail}:</strong> {msg.content}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default MessageList;
